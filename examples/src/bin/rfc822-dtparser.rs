29c2eaedcf55b4d6884e2a2eb61e6fcf43c9fb7f
Partha Susarla <mail@spartha.org>
2017-08-02 09:18:43 +1000

    Argument handling

--- examples/src/bin/rfc822-dtparser.rs
+++ examples/src/bin/rfc822-dtparser.rs
@@ -1,7 +1,15 @@
 extern crate rfc822dtparser as dtparser;
 
+use std::env;
 use dtparser::*;
 
 fn main() {
     dtparser::init().unwrap();
+
+    let args: Vec<_> = env::args().collect();
+    let dtstr: &str = if args.len() == 2 {
+        args[1].as_ref()
+    } else {
+        panic!("Usage: {} <date-time-string>", args[0]);
+    };
 }
